46b3376a9fba14fd91de6a34247b2ba9860547c0
DESKTOP-3SSGUTH\whdkf <whdkfk4012@naver.com>
2023-03-08 01:22:14 +0900

    no message

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,30 @@
+package hello.core;
+
+import hello.core.Order.OrderService;
+import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
